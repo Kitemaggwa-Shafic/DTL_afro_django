395e104ca27555471f275ec0ea13a8d5921659c2
KitemaggwaShafic <kitemaggwashafic@gmail.com>
2023-04-11 10:07:50 +0300

    DTL PROJECT DTL added new variables in views, url and referenced in index.html

--- afro_dj/afro_dj/urls.py
+++ afro_dj/afro_dj/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Including the path to the urls in our created afro_dtl App
+    path('', include('afro_dtl.urls')),
 ]
--- afro_dj/afro_dtl/views.py
+++ afro_dj/afro_dtl/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# My index view
+def index(request):
+    # initiating a variable to use
+    p_title = 'Afro Django Templating'
+    # rendering a template and diaplsy it with whate ever is to be passed on with a request
+    # we are sending a dic p_title a key and value p_title
+    return render(request, 'index.html', {'p_title': p_title})
