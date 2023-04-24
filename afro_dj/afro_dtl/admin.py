2b25b966f27dbd90676c015f090e0e83da52ed3f
KitemaggwaShafic <kitemaggwashafic@gmail.com>
2023-04-24 11:59:37 +0300

    DTL Added better flash messages with bootstrap CSS and JS timeout

--- afro_dj/afro_dtl/admin.py
+++ afro_dj/afro_dtl/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from . models import User_Signup, User_form_model
+from . models import User_Signup
 
 # Register your models here.
 
 admin.site.register(User_Signup),
-admin.site.register(User_form_model),
